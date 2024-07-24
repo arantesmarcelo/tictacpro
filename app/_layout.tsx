a1caf4ab91c544082acd2321d35c2a6e25215c1a
Marcelo Arantes <marcelo.delimaarantes@edu.sait.ca>
2024-07-24 17:50:54 -0600

    Downgrade to React 17 to fix the issue with antd version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,22 +7,33 @@ import {
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 import LoginPage from "./login";
 
+import auth from "@react-native-firebase/auth";
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  //firebase
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   const [loggedIn, setLoggedIn] = useState(false);
+
+  function onAuthStateChanged(user: any) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
 
   useEffect(() => {
     if (loaded) {
@@ -34,20 +45,23 @@ export default function RootLayout() {
     return null;
   }
 
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return <LoginPage onLogin={(loggedIn: boolean) => setLoggedIn(loggedIn)} />;
+  }
+
   return (
     <>
-      {loggedIn ? (
-        // <ThemeProvider
-        //   value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        // >
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-      ) : (
-        // </ThemeProvider>
-        <LoginPage onLogin={(loggedIn: boolean) => setLoggedIn(loggedIn)} />
-      )}
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
     </>
   );
 }
